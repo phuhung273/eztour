47f29414eb9e49406fe9cd1b3a5047db5e8f2882
phuhung273 <hung.tran@alttekglobal.com>
2021-09-03 22:14:35 +0700

    fix api route

--- cms_php/routes/api.php
+++ cms_php/routes/api.php
@@ -1,15 +1,14 @@
 <?php
 
 use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\Auth\MobileAuthController;
 use App\Http\Controllers\GreetingController;
 use App\Http\Controllers\LocationController;
 use App\Http\Controllers\MobileHomeController;
 use App\Http\Controllers\TodoController;
-use App\Models\User;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,55 +24,42 @@ use Illuminate\Validation\ValidationException;
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', fn(Request $request) => $request->user);
 
-    Route::name('locations.')->group(function() {
-        Route::prefix('locations')->group(function() {
-            Route::get('/', [LocationController::class, 'index'])->name('index');
+    Route::name('api.')->group(function() {
+
+        Route::name('locations.')->group(function() {
+            Route::prefix('locations')->group(function() {
+                Route::get('/', [LocationController::class, 'index'])->name('index');
+            });
+        });
+    
+        Route::name('greetings.')->group(function() {
+            Route::prefix('greetings')->group(function() {
+                Route::get('/', [GreetingController::class, 'index'])->name('index');
+            });
+        });
+    
+        Route::name('todos.')->group(function() {
+            Route::prefix('todos')->group(function() {
+                Route::get('/', [TodoController::class, 'index'])->name('index');
+            });
+        });
+    
+        Route::name('announcements.')->group(function() {
+            Route::prefix('announcements')->group(function() {
+                Route::get('/', [AnnouncementController::class, 'index'])->name('index');
+            });
+        });
+    
+        Route::name('home.')->group(function() {
+            Route::prefix('home')->group(function() {
+                Route::post('/', [MobileHomeController::class, 'index'])->name('index');
+            });
         });
     });
 
-    Route::name('greetings.')->group(function() {
-        Route::prefix('greetings')->group(function() {
-            Route::get('/', [GreetingController::class, 'index'])->name('index');
-        });
-    });
-
-    Route::name('todos.')->group(function() {
-        Route::prefix('todos')->group(function() {
-            Route::get('/', [TodoController::class, 'index'])->name('index');
-        });
-    });
-
-    Route::name('announcements.')->group(function() {
-        Route::prefix('announcements')->group(function() {
-            Route::get('/', [AnnouncementController::class, 'index'])->name('index');
-        });
-    });
-
-    Route::name('home.')->group(function() {
-        Route::prefix('home')->group(function() {
-            Route::post('/', [MobileHomeController::class, 'index'])->name('index');
-        });
-    });
 });
 
-Route::post('/login', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);
+Route::name('api.')->group(function() {
 
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return [
-        'access_token' => $user->createToken($request->device_name)->plainTextToken,
-        'user_id' => $user->id,
-        'user_name' => $user->email,
-    ];
+    Route::post('/login', [MobileAuthController::class, 'login'])->name('login');
 });
